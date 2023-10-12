1ef22ef6bf70ccf46a6260355318970ba672af36
unknown <Adeleyeha.17@protonmail.com>
2023-10-12 09:12:18 +0100

    nav update

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 
 export const Navbar = () => {
   return (
-    <div className="sm:flex justify-center items-center h-16 md:max-w-2xl lg:max-w-7xl mx-auto text-white">
+    <div className="sm:flex justify-center items-center h-16 md:max-xl:px-7 md:max-w-6xl lg:max-w-7xl mx-auto text-white">
       <h1 className="w-full text-3xl font-bold text-green">BigData</h1>
 
       <ul className="sm:flex">
