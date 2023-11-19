dcfbfd43bfb3cd0959eb23d2b98b82e5ea5eb3b7
unknown <Adeleyeha.17@protonmail.com>
2023-11-19 15:39:48 +0100

    Added text and padding to the Hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,12 +7,14 @@ export const Hero:React.FC = () => {
       <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
             <p className="text-[#00df9a] font-bold p-2">GROWING WITH DATA ANALYTICS</p>
             <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">Grow with data.</h1>
-            <div>
+            <div className="flex justify-center items-center gap-3 md:gap-5">
                <p className="md:text-5xl sm:text-4xl text-xl font-bold">
-                  flex, flexible financing for
+                  Fast, flexible financing for
                </p>
 
-            <Typed strings={[
+            <Typed
+            className="md:text-5xl sm:text-4xl text-xl font-bold py-4"
+             strings={[
                "BTB",
                "BTC",
                "SAS"
@@ -21,6 +23,8 @@ export const Hero:React.FC = () => {
             backSpeed={140}
             loop />
             </div>
+
+            <p className="md:text-2xl text-lg font-bold text-gray-500">Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
       </div>
   
     </div>
